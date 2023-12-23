48c725d6e123ef5a0c7e79c97ef0e0ab19e87f90
MichelangeloCoder <yadavdev080403@gmail.com>
2023-12-23 12:47:31 +0530

    lots of CoinDetail Model changes

--- app/src/main/java/com/example/coinapiapp/data/data_source/dtoModel/CoinDetailDTO/ConvertedVolume.kt
+++ app/src/main/java/com/example/coinapiapp/data/data_source/dtoModel/CoinDetailDTO/ConvertedVolume.kt
@@ -3,5 +3,5 @@ package com.example.coinapiapp.data.data_source.dtoModel.CoinDetailDTO
 data class ConvertedVolume(
     val btc: Double,
     val eth: Double,
-    val usd: Int
+    val usd: Double
 )
